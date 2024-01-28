789db609ecd7ee20154e2fd14d0f38bf1137aa1e
taylorlloyddavis <156040290+taylorlloyddavis@users.noreply.github.com>
2024-01-28 08:29:13 -0800

    Update HW4.js
    updated js

--- HW4.js
+++ HW4.js
@@ -1,5 +1,6 @@
- // define a Constructor function
- var Food = function(fName, fCalories) {
+// JavaScript source code
+// define a Constructor function
+var Food = function (fName, fCalories) {
     this.fName = fName;
     this.fCalories = parseInt(fCalories);
 };
@@ -19,29 +20,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // Function to update total calories
 function updateTotalCalories() {
-  const totalCaloriesElement = document.getElementById('totalCalories');
-  let totalCalories = 0;
+    const totalCaloriesElement = document.getElementById('totalCalories');
+    let totalCalories = 0;
 
-  foodArray.forEach(food => {
-    totalCalories += food.fCalories;
-  });
+    foodArray.forEach(food => {
+        totalCalories += food.fCalories;
+    });
 
-  totalCaloriesElement.textContent = totalCalories;
+    totalCaloriesElement.textContent = totalCalories;
 }
 
 // define what should happen when the form is submitted
 function formSubmitEvent() {
-  let foodName = document.getElementById("foodName").value;
-  let calories = document.getElementById("calories").value;
+    let foodName = document.getElementById("foodName").value;
+    let calories = document.getElementById("calories").value;
 
-  if (foodName.trim() !== '' && !isNaN(calories) && parseInt(calories) > 0) {
-    foodArray.push(new Food(foodName, calories));
-    console.log(foodArray); // write array into console
-    updateTotalCalories();
-    // clear text boxes
-    document.getElementById("foodName").value = "";
-    document.getElementById("calories").value = "";
-  } else {
-    alert('Please enter a food name and calorie amount.'); //displays message to enter food name and calorie amt
-  }
+    if (foodName.trim() !== '' && !isNaN(calories) && parseInt(calories) > 0) {
+        foodArray.push(new Food(foodName, calories));
+        console.log(foodArray); // write array into console
+        updateTotalCalories();
+        // clear text boxes
+        document.getElementById("foodName").value = "";
+        document.getElementById("calories").value = "";
+    } else {
+        alert('Please enter a food name and calorie amount.'); //displays message to enter food name and calorie amt
+    }
 }
